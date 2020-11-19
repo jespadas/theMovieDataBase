3bd5782ea04fd698db5b31ba710afb84dbea1fb7
jespadas <julio.espadas@gmail.com>
2020-11-19 22:58:46 +0100

    Entity Framework initial Database schema

---
+++ Data/TheMovieDataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TheMovieDataBase.Models;
+
+namespace TheMovieDataBase.Data
+{
+    public class TheMovieDataBaseContext : DbContext
+    {
+        public TheMovieDataBaseContext (DbContextOptions<TheMovieDataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TheMovieDataBase.Models.Movie> Movie { get; set; }
+    }
+}
