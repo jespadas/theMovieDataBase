bba2439b8d207fd558b38e43b3dad537d07d1ebe
jespadas <julio.espadas@gmail.com>
2020-11-22 19:59:45 +0100

    database initial config

---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TheMovieDataBase.Data;
+using System;
+using System.Linq;
+
+namespace TheMovieDataBase.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+               serviceProvider.GetRequiredService<
+                   DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 8M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 9M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 10M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 4M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
